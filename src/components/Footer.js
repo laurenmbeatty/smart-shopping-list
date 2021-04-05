48cbeea22c806aa846b6cab5ba0940fe89bdcbc1
Lauren Beatty <lauren.beatty@treasure-data.com>
2021-04-05 10:16:13 -0500

    add item to db

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,23 +1,25 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
 
-const Footer = () =>  {
-    return (
-        <footer>
-            <nav>
-                <ul className="footer">
-                    <li>
-                        <NavLink activeStyle={{fontWeight: 700}} exact to="/list">List</NavLink>
-
-                    </li>
-                    <li>
-                        <NavLink  activeStyle={{fontWeight: 700}} exact to="/add-item">Add Item</NavLink>
-
-                    </li>
-                </ul>
-            </nav>
-        </footer>
-    )
+const Footer = () => {
+  return (
+    <footer>
+      <nav>
+        <ul className="footer">
+          <li>
+            <NavLink activeStyle={{ fontWeight: 700 }} exact to="/list">
+              List
+            </NavLink>
+          </li>
+          <li>
+            <NavLink activeStyle={{ fontWeight: 700 }} exact to="/add-item">
+              Add Item
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </footer>
+  )
 }
 
 export default Footer
