ea3351f8111eabc49fdfb8319491ca41e9ceebbc
Lauren Beatty <lauren.beatty@treasure-data.com>
2021-04-05 14:48:37 -0500

    allow user to enter existing token

--- src/App.js
+++ src/App.js
@@ -10,9 +10,13 @@ import './App.css'
 import Footer from './components/Footer'
 import getToken from './lib/tokens'
 import List from './List'
+import Welcome from './components/Welcome'
+import { db } from './lib/firebase'
 
 function App() {
   const [token, setToken] = useState(null)
+  const [value, setValue] = useState('')
+  const [error, setError] = useState(false)
 
   useEffect(() => {
     const user = localStorage.getItem('userToken')
@@ -21,6 +25,28 @@ function App() {
 
   const handleClick = () => {
     setToken(localStorage.setItem('userToken', getToken()))
+    db.collection('userTokens').add({
+      tokenName: token,
+    })
+  }
+
+  const handleChange = (e) => {
+    setValue(e.target.value)
+    setError(false)
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const tokens = await db
+      .collection('userTokens')
+      .where('tokenName', '==', value)
+      .get()
+    if (tokens.empty) {
+      setError(true)
+    } else {
+      localStorage.setItem('userToken', value)
+      setToken(value)
+    }
   }
 
   return (
@@ -29,7 +55,16 @@ function App() {
         {!token && <button onClick={() => handleClick()}>Login</button>}
         <Switch>
           <Route exact path="/">
-            {token ? <Redirect to="/list" /> : <div>Home</div>}
+            {token ? (
+              <Redirect to="/list" />
+            ) : (
+              <Welcome
+                error={error}
+                onChange={(e) => handleChange(e)}
+                onSubmit={(e) => handleSubmit(e)}
+                token={value}
+              />
+            )}
           </Route>
           {token && (
             <>
